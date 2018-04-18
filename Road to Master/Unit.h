c60082e80c0a3bce9346de312a3bfd146f648f12
KotoriSN <pichchai11@gmail.com>
2018-04-18 13:52:02 +0700

    Update

--- Road to Master/Unit.h
+++ Road to Master/Unit.h
@@ -13,7 +13,32 @@ class Unit {
 	int lvl; // level.
 	int exp;
 	int Mexp; //max exp.
-	Equipment* weapon, offhand, armor, accessory;
-public:
-	Unit(string, int, int, int, int, int, int, int, int); // name,str,agi,dex,int,luk,vit,exp,money
+	//Equipment* weapon, offhand, armor, accessory;
+	public:
+		Unit(string, int, int, int, int, int, int, int, int); // name,str,agi,dex,int,luk,vit,exp,money
+		void statuscal();
+		void asdf();
 };
+
+Unit::Unit(string rname, int rstr, int ragi, int rdex, int rint, int rluk, int rvit, int rexp, int rmoney) {
+	name = rname;
+	str = rstr;
+	dex = rdex;
+	Int = rint;
+	luk = rluk;
+	vit = rvit;
+	exp = rexp;
+	money = rmoney;
+}
+
+void Unit::statuscal() {
+	Maxhp = 10 + (vit * 5);
+	Maxmp = 5 + (Int * 2);
+	atk = 2 + (str * 2);
+	matk = 2 + (Int * 2);
+	def = 2 + vit + str;
+	mdef = 2 + vit + Int;
+	cri = 1 + (luk * 0.5);
+	eva = agi * 1.5;
+	acc = dex;
+}
